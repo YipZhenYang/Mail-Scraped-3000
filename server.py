887c1492c9f5af4de03db0d182518f01d33a5398
YipZhenYang <yipzhenyang03@gmail.com>
2025-03-05 21:52:22 +0800

    Added favicon to fix 404 error

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import os
 from werkzeug.utils import secure_filename
 from flask_cors import CORS
 
-app = Flask(__name__, template_folder="templates")  # Ensure templates folder is used
+app = Flask(__name__, template_folder="templates", static_folder="static")  # Ensure templates folder is used
 CORS(app)
 
 UPLOAD_FOLDER = "uploads"
@@ -122,6 +122,5 @@ def download():
         return send_file(file_name, as_attachment=True)
     return "File not found", 404
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Get port from environment variable
-    app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
