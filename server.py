826a7e033eb2587758c486a95a4e7dd10a3d2bdd
YipZhenYang <yipzhenyang03@gmail.com>
2025-03-07 16:42:57 +0800

    Updated Mail Scraped 3000 Ver 1.2

--- server.py
+++ server.py
@@ -27,9 +27,9 @@ CORS(app)
 # Configuration
 UPLOAD_FOLDER = "uploads"
 ALLOWED_EXTENSIONS = {"csv"}
-MAX_WORKERS = 2
+MAX_WORKERS = 3
 MAX_UPLOAD_SIZE = 30 * 1024
-REQUEST_TIMEOUT = 600
+REQUEST_TIMEOUT = 900
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
